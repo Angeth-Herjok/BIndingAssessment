98a2755733bf4f7d21c985d9969a55151526d833
Angeth-Herjok <angethherjok@gmail.com>
2023-06-20 09:32:25 +0300

    first commit

--- app/src/main/java/com/example/assessment/Login.kt
+++ app/src/main/java/com/example/assessment/Login.kt
@@ -3,14 +3,16 @@ package com.example.assessment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup.Binding
 import com.example.assessment.databinding.ActivityLoginBinding
+
+import com.example.assessment.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
     }
 
     override fun onResume() {
