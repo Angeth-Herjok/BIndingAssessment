838c9d03685b3d9170ffffffb4a1c6d6d83624a8
Angeth-Herjok <angethherjok@gmail.com>
2023-07-17 16:05:59 +0300

    first commit

--- app/src/main/java/com/example/assessment/Login.kt
+++ app/src/main/java/com/example/assessment/Login.kt
@@ -3,6 +3,7 @@ package com.example.assessment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.assessment.databinding.ActivityLoginBinding
 
 import com.example.assessment.databinding.ActivityMainBinding
@@ -23,6 +24,47 @@ class Login : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+    fun validateSignUp() {
+        binding.tilEmail.error=null
+        binding.tilPassword.error=null
+//        binding.tilConfirmPassword.error=null
+
+
+        val firstName = binding.etEmail.text.toString()
+        val lastName = binding.etPassword.text.toString()
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+//        val passwordConfirmation = binding.etConfirmPassword.text.toString()
+        var error=false
+
+        if (firstName.isBlank()) {
+            binding.tilEmail.error = "Valid email required"
+            error=true
+        }
+//        if (lastName.isBlank()) {
+//            binding.tilEmail.error = "first name required"
+//            error=true
+//        }
+//        if (email.isBlank()) {
+//            binding.tilEmail.error = "first name required"
+//            error=true
+//        }
+        if (password.isBlank()) {
+            binding.tilPassword.error = "Correct password required"
+            error=true
+        }
+//        if (passwordConfirmation.isBlank()) {
+//            binding.tilConfirmPassword.error = "Correct password required"
+//            error=true
+//        }
+//        if (password!=passwordConfirmation){
+//            binding.tilConfirmPassword.error="password confirmation does not match password"
+//            error=true
+//        }
+        if (!error){
+            Toast.makeText(this,"$email $password", Toast.LENGTH_LONG).show()
+        }
     }
 
 }
--- app/src/main/java/com/example/assessment/SignUP.kt
+++ app/src/main/java/com/example/assessment/SignUP.kt
@@ -1,11 +1,68 @@
 package com.example.assessment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.assessment.databinding.ActivityLoginBinding
+import com.example.assessment.databinding.ActivitySignUpBinding
 
 class SignUP : AppCompatActivity() {
+    lateinit var binding:ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        binding= ActivitySignUpBinding.inflate(layoutInflater)
+    }
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
+        binding.btnSignUP.setOnClickListener {
+            val intent = Intent(this, SignUP::class.java)
+            startActivity(intent)
+        }
+        fun validateSignUp() {
+            binding.tilEmail.error = null
+            binding.tilPassword.error = null
+            binding.tilPhone.error = null
+            binding.tilUserName.error = null
+
+
+            val phone = binding.etPhone.text.toString()
+//        val lastName = binding.etPassword.text.toString()
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            val userName = binding.etUserName.text.toString()
+            var error = false
+
+            if (userName.isBlank()) {
+                binding.tilUserName.error = "Valid email required"
+                error = true
+            }
+//        if (lastName.isBlank()) {
+//            binding.tilEmail.error = "first name required"
+//            error=true
+//        }
+//        if (email.isBlank()) {
+//            binding.tilEmail.error = "first name required"
+//            error=true
+//        }
+            if (password.isBlank()) {
+                binding.tilPassword.error = "Correct password required"
+                error = true
+            }
+            if (phone.isBlank()) {
+                binding.tilPhone.error = "Correct phone required"
+                error = true
+            }
+//        if (password!=passwordConfirmation){
+//            binding.tilConfirmPassword.error="password confirmation does not match password"
+//            error=true
+//        }
+            if (!error) {
+                Toast.makeText(this, "$email $password", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
